622677ecb2f3b8387d7b637e3394f0cd04227a8d
gary.wong <Gary.Wong@ticketmaster.com>
2014-09-23 11:44:22 -0700

    Move FakeServerSocket into test folder

--- src/test/java/FakeServerSocket.java
+++ src/test/java/FakeServerSocket.java
@@ -1,10 +1,4 @@
-package com.carebears;
-
 import com.carebears.CareBearSocket;
-
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 public class FakeServerSocket implements CareBearSocket {
     public boolean started;
--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -1,23 +1,19 @@
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import com.carebears.*;
-
-
-import java.net.ServerSocket;
-import java.net.Socket;
+import com.carebears.Server;
 
 public class ServerTest {
 
 
     @Test
     public void itWillReturnThePortNumber() throws Exception {
-//        ServerSocket socket1 = new com.carebears.FakeServerSocket();
+//        ServerSocket socket1 = new java.FakeServerSocket();
 //        int port1 = socket1.getLocalPort();
 //
 //        assertEquals(5000, port1);
 //
-//        ServerSocket socket2 = new com.carebears.FakeServerSocket();
+//        ServerSocket socket2 = new java.FakeServerSocket();
 //        int port2 = socket2.getLocalPort();
 //
 //        assertEquals(5100, port2);
