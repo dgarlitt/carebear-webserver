823eb2cbef44ad5f3fc5630ce266b80a0bf98518
gary.wong <Gary.Wong@ticketmaster.com>
2014-09-23 13:40:48 -0700

    Add http Handler test

---
+++ src/test/java/httpHandlerTest.java
@@ -0,0 +1,32 @@
+import com.carebears.CareBearHttpHandler;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class httpHandlerTest {
+    CareBearHttpHandler handler;
+
+    @Before
+    public void setsUpHttpHandler() {
+        handler = new CareBearHttpHandler();
+    }
+    @Test
+    public void ItHandlesRequest() throws Exception {
+        String stringResponse = handler.handle("GET /");
+        assertEquals("HTTP/1.0 200", stringResponse);
+    }
+
+    @Test
+    public void ItHandles404Request() throws Exception {
+        String stringResponse = handler.handle("GET /foobar");
+        assertEquals("HTTP/1.0 404", stringResponse);
+    }
+
+    @Test
+    public void ItHandlesUnreconizedRequest() throws Exception {
+        String stringResponse = handler.handle("POST /");
+        assertEquals("HTTP/1.0 404", stringResponse);
+    }
+
+}
